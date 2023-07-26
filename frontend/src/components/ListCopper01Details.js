797db7aecfa55fe786e55e425497f7552ecd6046
rearl89 <rogerearl@me.com>
2023-07-26 09:51:54 -0700

    Made list copper01 details more visually functional.

--- frontend/src/components/ListCopper01Details.js
+++ frontend/src/components/ListCopper01Details.js
@@ -3,43 +3,43 @@ import format from 'date-fns/format'
 const ListCopper01Details = ({copper01}) => {
     return (
         <div className="listContainer">       
-                    <p>&nbsp;{copper01.batchID}</p>
-                    <p>{copper01.set}</p>
+                    <p>&nbsp;<strong>{copper01.batchID}</strong></p>
+                    <p><strong>{copper01.set}</strong></p>
                     <p>{format(new Date(copper01.createdAt), 'MM-dd-yyyy')}</p>
                     <p>{copper01.postPass}</p>
                     <p>{copper01.spongePass}</p>
                     <p>{copper01.rng1b}</p>
                     <p>{copper01.rng1m}</p>
                     <p>{copper01.rng1t}</p>
-                    <p>{copper01.rng1Average}</p>
+                    <p><strong>{copper01.rng1Average}</strong></p>
                     <p>{copper01.rng2b}</p>
                     <p>{copper01.rng2m}</p>
                     <p>{copper01.rng2t}</p>
-                    <p>{copper01.rng2Average}</p>
+                    <p><strong>{copper01.rng2Average}</strong></p>
                     <p>{copper01.rng3b}</p>
                     <p>{copper01.rng3m}</p>
                     <p>{copper01.rng3t}</p>
-                    <p>{copper01.rng3Average}</p>
+                    <p><strong>{copper01.rng3Average}</strong></p>
                     <p>{copper01.rng4b}</p>
                     <p>{copper01.rng4m}</p>
                     <p>{copper01.rng4t}</p>
-                    <p>{copper01.rng4Average}</p>
+                    <p><strong>{copper01.rng4Average}</strong></p>
                     <p>{copper01.rng1od1}</p>
                     <p>{copper01.rng1od2}</p>
-                    <p>{copper01.rng1odAverage}</p>
+                    <p><strong>{copper01.rng1odAverage}</strong></p>
                     <p>{copper01.rng2od1}</p>
                     <p>{copper01.rng2od2}</p>
-                    <p>{copper01.rng2odAverage}</p>
+                    <p><strong>{copper01.rng2odAverage}</strong></p>
                     <p>{copper01.rng3od1}</p>
                     <p>{copper01.rng3od2} </p>
                     <p>{copper01.rng3od3}</p>
                     <p>{copper01.rng3od4} </p>
-                    <p>{copper01.rng3odAverage}</p>
+                    <p><strong>{copper01.rng3odAverage}</strong></p>
                     <p>{copper01.rng4od1}</p>
                     <p>{copper01.rng4od2}</p>
                     <p>{copper01.rng4od3}</p>
                     <p>{copper01.rng4od4}</p>
-                    <p>{copper01.rng4odAverage}</p>    
+                    <p><strong>{copper01.rng4odAverage}</strong></p>    
         </div>
     )
 }
